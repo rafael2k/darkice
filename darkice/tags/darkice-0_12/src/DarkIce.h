126224db09f68f9b68630f56d8532b5b0a350fd3
nobody <nobody@caa462fc-51d2-11de-8b28-e1b452efc53a>
2002-10-20 21:28:25 +0000

    This commit was manufactured by cvs2svn to create tag 'darkice-0_12'.

---
+++ darkice/tags/darkice-0_12/src/DarkIce.h
@@ -0,0 +1,362 @@
+/*------------------------------------------------------------------------------
+
+   Copyright (c) 2000 Tyrell Corporation. All rights reserved.
+
+   Tyrell DarkIce
+
+   File     : DarkIce.h
+   Version  : $Revision$
+   Author   : $Author$
+   Location : $Source$
+   
+   Copyright notice:
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License  
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+------------------------------------------------------------------------------*/
+#ifndef DARK_ICE_H
+#define DARK_ICE_H
+
+#ifndef __cplusplus
+#error This is a C++ include file
+#endif
+
+
+/* ============================================================ include files */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#else
+#error need unistd.h
+#endif
+
+#include <iostream>
+
+#include "Referable.h"
+#include "Reporter.h"
+#include "Exception.h"
+#include "Ref.h"
+#include "AudioSource.h"
+#include "BufferedSink.h"
+#include "Connector.h"
+#include "AudioEncoder.h"
+#include "TcpSocket.h"
+#include "CastSink.h"
+#include "Config.h"
+
+
+/* ================================================================ constants */
+
+
+/* =================================================================== macros */
+
+
+/* =============================================================== data types */
+
+/**
+ *  Program main object.
+ *
+ *  @author  $Author$
+ *  @version $Revision$
+ */
+class DarkIce : public virtual Referable, public virtual Reporter
+{
+    private:
+
+        /**
+         *  The maximum number of supported outputs. This should be
+         *  <supported output types> * <outputs per type>
+         */
+        static const unsigned int       maxOutput = 4 * 7;
+        
+        /**
+         *  Type describing each lame library output.
+         */
+        typedef struct {
+            Ref<AudioEncoder>       encoder;
+            Ref<TcpSocket>          socket;
+            Ref<CastSink>           server;
+        } Output;
+
+        /**
+         *  The outputs.
+         */
+        Output                  audioOuts[maxOutput];
+
+        /**
+         *  Number of lame library outputs.
+         */
+        unsigned int            noAudioOuts;
+
+        /**
+         *  Duration of playing, in seconds.
+         */
+        unsigned int            duration;
+
+        /**
+         *  The dsp to record from.
+         */
+        Ref<AudioSource>        dsp;
+
+        /**
+         *  The encoding Connector, connecting the dsp to the encoders.
+         */
+        Ref<Connector>          encConnector;
+
+        /**
+         *  Original scheduling policy
+         */
+        int                     origSchedPolicy;
+
+        /**
+         *  Original scheduling priority
+         */
+        int                     origSchedPriority;
+
+        /**
+         *  Initialize the object.
+         *
+         *  @param config the config Object to read initialization
+         *                information from.
+         *  @exception Exception
+         */
+        void
+        init (  const Config   & config )            throw ( Exception );
+
+        /**
+         *  Look for the icecast stream outputs from the config file.
+         *  Called from init()
+         *
+         *  @param config the config Object to read initialization
+         *                information from.
+         *  @param bufferSecs number of seconds to buffer audio for
+         *  @exception Exception
+         */
+        void
+        configIceCast (  const Config   & config,
+                         unsigned int     bufferSecs  )     throw ( Exception );
+
+        /**
+         *  Look for the icecast2 stream outputs from the config file.
+         *  Called from init()
+         *
+         *  @param config the config Object to read initialization
+         *                information from.
+         *  @param bufferSecs number of seconds to buffer audio for
+         *  @exception Exception
+         */
+        void
+        configIceCast2 (  const Config   & config,
+                          unsigned int     bufferSecs  )    throw ( Exception );
+
+        /**
+         *  Look for the shoutcast stream outputs from the config file.
+         *  Called from init()
+         *
+         *  @param config the config Object to read initialization
+         *                information from.
+         *  @param bufferSecs number of seconds to buffer audio for
+         *  @exception Exception
+         */
+        void
+        configShoutCast (   const Config   & config,
+                            unsigned int     bufferSecs )   throw ( Exception );
+
+        /**
+         *  Look for file outputs from the config file.
+         *  Called from init()
+         *
+         *  @param config the config Object to read initialization
+         *                information from.
+         *  @exception Exception
+         */
+        void
+        configFileCast  (   const Config   & config )
+                                                            throw ( Exception );
+
+        /**
+         *  Set POSIX real-time scheduling for the encoding process,
+         *  if user permissions enable it.
+         *
+         *  @exception Exception
+         */
+        void
+        setRealTimeScheduling ( void )              throw ( Exception );
+
+        /**
+         *  Set the scheduling that was before setting real-time scheduling.
+         *  This function must be called _only_ after setRealTimeScheduling.
+         *
+         *  @exception Exception
+         */
+        void
+        setOriginalScheduling ( void )              throw ( Exception );
+
+        /**
+         *  Start encoding. Spawns all encoders, opens the dsp and
+         *  starts sending data to the encoders.
+         *
+         *  @return if encoding was successful.
+         *  @exception Exception
+         */
+        bool
+        encode ( void )                             throw ( Exception );
+
+        /**
+         *  Start shouting. fork()-s a process for each output, reads
+         *  the output of the encoders and sends them to an IceCast server.
+         *
+         *  @return if shouting was successful.
+         *  @exception Exception
+         */
+        bool
+        shout ( unsigned int )                      throw ( Exception );
+
+
+    protected:
+
+        /**
+         *  Default constructor. Always throws an Exception.
+         *
+         *  @exception Exception
+         */
+        inline
+        DarkIce ( void )                            throw ( Exception )
+        {
+            throw Exception( __FILE__, __LINE__);
+        }
+
+
+    public:
+
+        /**
+         *  Constructor based on a configuration object.
+         *
+         *  @param config the config Object to read initialization
+         *                information from.
+         *  @exception Exception
+         */
+        inline
+        DarkIce (   const Config  & config )       throw ( Exception )
+        {
+            init( config);
+        }
+
+        /**
+         *  Destructor.
+         *
+         *  @exception Exception
+         */
+        inline virtual
+        ~DarkIce ( void )                           throw ( Exception )
+        {
+        }
+
+/* TODO
+
+        inline
+        DarkIce ( const DarkIce   & di )            throw ( Exception )
+        {
+        }
+
+
+        inline DarkIce &
+        operator= ( const DarkIce   * di )          throw ( Exception )
+        {
+        }
+*/
+
+        /**
+         *  Run the process of recording / encoding / sending to the servers.
+         *
+         *  @return 0 on success
+         *  @exception Exception
+         */
+        virtual int
+        run ( void )                                throw ( Exception );
+
+};
+
+
+/* ================================================= external data structures */
+
+
+/* ====================================================== function prototypes */
+
+
+
+#endif  /* DARK_ICE_H */
+
+
+/*------------------------------------------------------------------------------
+ 
+  $Source$
+
+  $Log$
+  Revision 1.14  2002/05/28 12:35:41  darkeye
+  code cleanup: compiles under gcc-c++ 3.1, using -pedantic option
+
+  Revision 1.13  2002/02/28 09:49:25  darkeye
+  added possibility to save the encoded stream to a local file only
+  (no streaming server needed)
+
+  Revision 1.12  2001/09/14 19:31:06  darkeye
+  added IceCast2 / vorbis support
+
+  Revision 1.11  2001/09/11 15:05:21  darkeye
+  added Solaris support
+
+  Revision 1.10  2001/09/09 11:27:31  darkeye
+  added support for ShoutCast servers
+
+  Revision 1.9  2001/08/30 17:25:56  darkeye
+  renamed configure.h to config.h
+
+  Revision 1.8  2001/08/26 20:44:30  darkeye
+  removed external command-line encoder support
+  replaced it with a shared-object support for lame with the possibility
+  of static linkage
+
+  Revision 1.7  2000/12/20 12:36:47  darkeye
+  added POSIX real-time scheduling
+
+  Revision 1.6  2000/11/15 18:08:43  darkeye
+  added multiple verbosity-level event reporting and verbosity command
+  line option
+
+  Revision 1.5  2000/11/13 19:38:55  darkeye
+  moved command line parameter parsing from DarkIce.cpp to main.cpp
+
+  Revision 1.4  2000/11/13 18:46:50  darkeye
+  added kdoc-style documentation comments
+
+  Revision 1.3  2000/11/10 20:16:21  darkeye
+  first real tests with multiple streaming
+
+  Revision 1.2  2000/11/09 22:09:46  darkeye
+  added multiple outputs
+  added configuration reading
+  added command line processing
+
+  Revision 1.1.1.1  2000/11/05 10:05:50  darkeye
+  initial version
+
+  
+------------------------------------------------------------------------------*/
+
---
+++ darkice/tags/darkice-0_12/src/IceCast2.cpp
@@ -0,0 +1,288 @@
+/*------------------------------------------------------------------------------
+
+   Copyright (c) 2000 Tyrell Corporation. All rights reserved.
+
+   Tyrell DarkIce
+
+   File     : IceCast2.cpp
+   Version  : $Revision$
+   Author   : $Author$
+   Location : $Source$
+   
+   Copyright notice:
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License  
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+------------------------------------------------------------------------------*/
+
+/* ============================================================ include files */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_STDIO_H
+#include <stdio.h>
+#else
+#error need stdio.h
+#endif
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#else
+#error need string.h
+#endif
+
+#ifdef HAVE_MATH_H
+#include <math.h>
+#else
+#error need math.h
+#endif
+
+
+#include "Exception.h"
+#include "Source.h"
+#include "Sink.h"
+#include "Util.h"
+#include "IceCast2.h"
+
+
+/* ===================================================  local data structures */
+
+
+/* ================================================  local constants & macros */
+
+/*------------------------------------------------------------------------------
+ *  File identity
+ *----------------------------------------------------------------------------*/
+static const char fileid[] = "$Id$";
+
+
+/*------------------------------------------------------------------------------
+ *  Size of string conversion buffer
+ *----------------------------------------------------------------------------*/
+#define STRBUF_SIZE         32
+
+
+/*------------------------------------------------------------------------------
+ *  Expected positive response from server begins like this.
+ *----------------------------------------------------------------------------*/
+static const char responseOK[] = "HTTP/1.0 200";
+
+
+/* ===============================================  local function prototypes */
+
+
+/* =============================================================  module code */
+
+/*------------------------------------------------------------------------------
+ *  Initialize the object
+ *----------------------------------------------------------------------------*/
+void
+IceCast2 :: init (  StreamFormat            format,
+                    const char            * mountPoint,
+                    const char            * description )
+                                                        throw ( Exception )
+{
+    this->format         = format;
+    this->mountPoint     = Util::strDup( mountPoint);
+    this->description    = description    ? Util::strDup( description) : 0;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  De-initialize the object
+ *----------------------------------------------------------------------------*/
+void
+IceCast2 :: strip ( void )                           throw ( Exception )
+{
+    delete[] mountPoint;
+    if ( description ) {
+        delete[] description;
+    }
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Log in to the IceCast2 server
+ *----------------------------------------------------------------------------*/
+bool
+IceCast2 :: sendLogin ( void )                           throw ( Exception )
+{
+    Sink          * sink   = getSink();
+    Source        * source = getSocket();
+    const char    * str;
+    char            resp[STRBUF_SIZE];
+    unsigned int    len;
+    unsigned int    lenExpected;
+
+    if ( !source->isOpen() ) {
+        return false;
+    }
+    if ( !sink->isOpen() ) {
+        return false;
+    }
+
+    // send the request, a string like:
+    // "SOURCE <mountpoint> ICE/1.0"
+    str = "SOURCE /";
+    sink->write( str, strlen( str));
+    str = getMountPoint();
+    sink->write( str, strlen( str));
+    str = " HTTP/1.0";
+    sink->write( str, strlen( str));
+
+    // send the content type, Ogg Vorbis
+    str = "\nContent-type: ";
+    sink->write( str, strlen( str));
+    switch ( format ) {
+        case mp3:
+            str = "audio/mpeg";
+            break;
+
+        case oggVorbis:
+            str = "application/x-ogg";
+            break;
+
+        default:
+            throw Exception( __FILE__, __LINE__,
+                             "unsupported stream format", format);
+            break;
+    }
+    sink->write( str, strlen( str));
+
+    // send the authentication info
+    str = "\nAuthorization: Basic ";
+    sink->write( str, strlen(str));
+    {
+        // send source:<password> encoded as base64
+        char        * source = "source:";
+        const char  * pwd    = getPassword();
+        char        * tmp    = new char[Util::strLen(source) +
+                                        Util::strLen(str) + 1];
+        Util::strCpy( tmp, source);
+        Util::strCat( tmp, pwd);
+        char  * base64 = Util::base64Encode( tmp);
+        delete[] tmp;
+        sink->write( base64, strlen(base64));
+        delete[] base64;
+    }
+
+    // send user agent info
+    str = "\nUser-Agent: DarkIce/" VERSION " (http://darkice.sourceforge.net/)";
+    sink->write( str, strlen( str));
+
+    // send the ice- headers
+    str = "\nice-bitrate: ";
+    sink->write( str, strlen( str));
+    if ( log10(getBitRate()) >= (STRBUF_SIZE-2) ) {
+        throw Exception( __FILE__, __LINE__,
+                         "bitrate does not fit string buffer", getBitRate());
+    }
+    sprintf( resp, "%d", getBitRate());
+    sink->write( resp, strlen( resp));
+
+    str = "\nice-public: ";
+    sink->write( str, strlen( str));
+    str = getIsPublic() ? "1" : "0";
+    sink->write( str, strlen( str));
+
+    if ( getName() ) {
+        str = "\nice-name: ";
+        sink->write( str, strlen( str));
+        str = getName();
+        sink->write( str, strlen( str));
+    }
+
+    if ( getDescription() ) {
+        str = "\nice-description: ";
+        sink->write( str, strlen( str));
+        str = getDescription();
+        sink->write( str, strlen( str));
+    }
+
+    if ( getUrl() ) {
+        str = "\nice-url: ";
+        sink->write( str, strlen( str));
+        str = getUrl();
+        sink->write( str, strlen( str));
+    }
+
+    if ( getGenre() ) {
+        str = "\nice-genre: ";
+        sink->write( str, strlen( str));
+        str = getGenre();
+        sink->write( str, strlen( str));
+    }
+
+    str = "\n\n";
+    sink->write( str, strlen( str));
+    sink->flush();
+
+    // read the response, expected response begins with responseOK
+    lenExpected = Util::strLen( responseOK);
+    if ( (len = source->read( resp, STRBUF_SIZE-1)) < lenExpected ) {
+        return false;
+    }
+    resp[lenExpected] = 0;
+    if ( !Util::strEq( resp, responseOK) ) {
+        return false;
+    }
+    
+    // suck anything that the other side has to say
+    while ( source->canRead( 0, 0) && 
+           (len = source->read( resp, STRBUF_SIZE-1)) );
+
+    return true;
+}
+
+
+
+/*------------------------------------------------------------------------------
+ 
+  $Source$
+
+  $Log$
+  Revision 1.9  2002/10/20 21:09:35  darkeye
+  added processing of server response
+
+  Revision 1.8  2002/08/20 20:16:59  darkeye
+  added User-Agent header to HTTP login
+
+  Revision 1.7  2002/08/20 19:34:43  darkeye
+  minor fix
+
+  Revision 1.6  2002/08/20 18:39:13  darkeye
+  added HTTP Basic authentication for icecast2 logins
+
+  Revision 1.5  2002/05/28 12:35:41  darkeye
+  code cleanup: compiles under gcc-c++ 3.1, using -pedantic option
+
+  Revision 1.4  2002/02/20 10:35:35  darkeye
+  updated to work with Ogg Vorbis libs rc3 and current IceCast2 cvs
+
+  Revision 1.3  2002/02/19 15:24:26  darkeye
+  send Content-type header when logging in to icecast2 servers
+
+  Revision 1.2  2001/11/20 09:06:18  darkeye
+  fixed public stream reporting
+
+  Revision 1.1  2001/09/14 19:31:06  darkeye
+  added IceCast2 / vorbis support
+
+
+  
+------------------------------------------------------------------------------*/
+
