fb07aa020dc9e13d8666553590323d0b53ae1337
nobody <nobody@caa462fc-51d2-11de-8b28-e1b452efc53a>
2003-02-09 15:09:41 +0000

    This commit was manufactured by cvs2svn to create tag 'darkice-0_13'.

---
+++ darkice/tags/darkice-0_13/src/MultiThreadedConnector.cpp
@@ -0,0 +1,364 @@
+/*------------------------------------------------------------------------------
+
+   Copyright (c) 2000 Tyrell Corporation. All rights reserved.
+
+   Tyrell DarkIce
+
+   File     : MultiThreadedConnector.cpp
+   Version  : $Revision$
+   Author   : $Author$
+   Location : $Source$
+   
+   Copyright notice:
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License  
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+------------------------------------------------------------------------------*/
+
+/* ============================================================ include files */
+
+#include "Exception.h"
+#include "MultiThreadedConnector.h"
+
+
+/* ===================================================  local data structures */
+
+
+/* ================================================  local constants & macros */
+
+/*------------------------------------------------------------------------------
+ *  File identity
+ *----------------------------------------------------------------------------*/
+static const char fileid[] = "$Id$";
+
+
+/* ===============================================  local function prototypes */
+
+
+/* =============================================================  module code */
+
+/*------------------------------------------------------------------------------
+ *  Initialize the object
+ *----------------------------------------------------------------------------*/
+void
+MultiThreadedConnector :: init ( void )                 throw ( Exception )
+{
+    pthread_mutex_init( &mutexProduce, 0);
+    pthread_cond_init( &condProduce, 0);
+    threads = 0;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  De-initialize the object
+ *----------------------------------------------------------------------------*/
+void
+MultiThreadedConnector :: strip ( void )                throw ( Exception )
+{
+    if ( threads ) {
+        delete[] threads;
+        threads = 0;
+    }
+
+    pthread_cond_destroy( &condProduce);
+    pthread_mutex_destroy( &mutexProduce);
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Constructor
+ *----------------------------------------------------------------------------*/
+MultiThreadedConnector :: MultiThreadedConnector (
+                                const MultiThreadedConnector &   connector )
+                                                            throw ( Exception )
+            : Connector( connector)
+{
+    mutexProduce    = connector.mutexProduce;
+    condProduce     = connector.condProduce;
+
+    if ( threads ) {
+        delete[] threads;
+    }
+    threads = new ThreadData[numSinks];
+    for ( unsigned int  i = 0; i < numSinks; ++i ) {
+        threads[i] = connector.threads[i];
+    }
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Assignment operator
+ *----------------------------------------------------------------------------*/
+MultiThreadedConnector &
+MultiThreadedConnector :: operator= ( const MultiThreadedConnector & connector )
+                                                            throw ( Exception )
+{
+    if ( this != &connector ) {
+        Connector::operator=( connector);
+
+        mutexProduce    = connector.mutexProduce;
+        condProduce     = connector.condProduce;
+
+        if ( threads ) {
+            delete[] threads;
+        }
+        threads = new ThreadData[numSinks];
+        for ( unsigned int  i = 0; i < numSinks; ++i ) {
+            threads[i] = connector.threads[i];
+        }
+    }
+
+    return *this;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Open the source and all the sinks if needed
+ *  Create the sink threads
+ *----------------------------------------------------------------------------*/
+bool
+MultiThreadedConnector :: open ( void )                     throw ( Exception )
+{
+    unsigned int        i;
+
+    if ( !Connector::open() ) {
+        return false;
+    }
+
+    running = true;
+
+    pthread_attr_init( &threadAttr);
+    pthread_attr_setdetachstate( &threadAttr, PTHREAD_CREATE_JOINABLE);
+
+    threads = new ThreadData[numSinks];
+    for ( i = 0; i < numSinks; ++i ) {
+        ThreadData    * threadData = threads + i;
+
+        threadData->connector = this;
+        threadData->ixSink    = i;
+        threadData->accepting = true;
+        threadData->isDone    = true;
+        if ( pthread_create( &(threadData->thread),
+                             &threadAttr,
+                             ThreadData::threadFunction,
+                             threadData ) ) {
+            break;
+        }
+    }
+
+    // if could not create all, delete the ones created
+    if ( i < numSinks ) {
+        unsigned int    j;
+
+        // signal to stop for all running threads
+        pthread_mutex_lock( &mutexProduce);
+        running = false;
+        pthread_cond_broadcast( &condProduce);
+        pthread_mutex_unlock( &mutexProduce);
+
+        for ( j = 0; j < i; ++j ) {
+            pthread_join( threads[j].thread, 0);
+        }
+
+        delete[] threads;
+        threads = 0;
+
+        return false;
+    }
+
+    return true;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Transfer some data from the source to the sink
+ *----------------------------------------------------------------------------*/
+unsigned int
+MultiThreadedConnector :: transfer ( unsigned long       bytes,
+                                     unsigned int        bufSize,
+                                     unsigned int        sec,
+                                     unsigned int        usec )
+                                                            throw ( Exception )
+{   
+    unsigned int        b;
+
+    if ( numSinks == 0 ) {
+        return 0;
+    }
+
+    if ( bufSize == 0 ) {
+        return 0;
+    }
+
+    dataBuffer   = new unsigned char[bufSize];
+    dataSize     = 0;
+
+    reportEvent( 6, "MultiThreadedConnector :: tranfer, bytes", bytes);
+
+    for ( b = 0; !bytes || b < bytes; ) {
+        if ( source->canRead( sec, usec) ) {
+            unsigned int        i;
+
+            pthread_mutex_lock( &mutexProduce);
+            dataSize = source->read( dataBuffer, bufSize);
+            b       += dataSize;
+
+            // check for EOF
+            if ( dataSize == 0 ) {
+                reportEvent( 3, "MultiThreadedConnector :: transfer, EOF");
+                pthread_mutex_unlock( &mutexProduce);
+                break;
+            }
+
+            for ( i = 0; i < numSinks; ++i ) {
+                threads[i].isDone = false;
+            }
+
+            // tell sink threads that there is some data available
+            pthread_cond_broadcast( &condProduce);
+
+            // wait for all sink threads to get done with this data
+            while ( true ) {
+                for ( i = 0; i < numSinks && threads[i].isDone; ++i );
+                if ( i == numSinks ) {
+                    break;
+                }
+                pthread_cond_wait( &condProduce, &mutexProduce);
+            }
+            pthread_mutex_unlock( &mutexProduce);
+        } else {
+            reportEvent( 3, "MultiThreadedConnector :: transfer, can't read");
+            break;
+        }
+    }
+
+    delete[] dataBuffer;
+    return b;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  The function for each thread.
+ *  Read the presented data
+ *----------------------------------------------------------------------------*/
+void
+MultiThreadedConnector :: sinkThread( int       ixSink )
+{
+    ThreadData    * threadData = &threads[ixSink];
+    Sink          * sink       = sinks[ixSink].get();
+
+    while ( running ) {
+        // wait for some data to become available
+        pthread_mutex_lock( &mutexProduce);
+        while ( running && threadData->isDone ) {
+            pthread_cond_wait( &condProduce, &mutexProduce);
+        }
+        if ( !running ) {
+            pthread_mutex_unlock( &mutexProduce);
+            break;
+        }
+
+        if ( threadData->accepting ) {
+            if ( sink->canWrite( 0, 0) ) {
+                try {
+                    sink->write( dataBuffer, dataSize);
+                } catch ( Exception     & e ) {
+                    // something wrong. don't accept more data, try to
+                    // reopen the sink next time around
+                    threadData->accepting = false;
+                }
+            } else {
+                reportEvent( 4,
+                            "MultiThreadedConnector :: sinkThread can't write ",
+                             ixSink);
+                // don't care if we can't write
+            }
+        }
+        threadData->isDone = true;
+        pthread_cond_broadcast( &condProduce);
+        pthread_mutex_unlock( &mutexProduce);
+
+        if ( !threadData->accepting ) {
+            // if we're not accepting, try to reopen the sink
+            try {
+                sink->close();
+                sink->open();
+                threadData->accepting = sink->isOpen();
+            } catch ( Exception   & e ) {
+                // don't care, just try and try again
+            }
+        }
+    }
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Stop the treads
+ *  Close the source and all the sinks if needed
+ *----------------------------------------------------------------------------*/
+void
+MultiThreadedConnector :: close ( void )                    throw ( Exception )
+{
+    unsigned int    i;
+
+    // signal to stop for all threads
+    pthread_mutex_lock( &mutexProduce);
+    running = false;
+    pthread_cond_broadcast( &condProduce);
+    pthread_mutex_unlock( &mutexProduce);
+
+    // wait for all the threads to finish
+    for ( i = 0; i < numSinks; ++i ) {
+        pthread_join( threads[i].thread, 0);
+    }
+    pthread_attr_destroy( &threadAttr);
+
+    Connector::close();
+}
+
+
+/*------------------------------------------------------------------------------
+ *  The thread function
+ *----------------------------------------------------------------------------*/
+void *
+MultiThreadedConnector :: ThreadData :: threadFunction( void  * param )
+{
+    ThreadData     * threadData = (ThreadData*) param;
+    threadData->connector->sinkThread( threadData->ixSink);
+
+    return 0;
+}
+
+
+/*------------------------------------------------------------------------------
+ 
+  $Source$
+
+  $Log$
+  Revision 1.3  2002/10/20 20:43:17  darkeye
+  more graceful reconnect
+
+  Revision 1.2  2002/10/19 13:35:21  darkeye
+  when a connection is dropped, DarkIce tries to reconnect, indefinitely
+  removed extreme event reporting for thread-related events
+
+  Revision 1.1  2002/10/19 12:25:47  darkeye
+  changed internals so that now each encoding/server connection is
+  a separate thread
+
+
+  
+------------------------------------------------------------------------------*/
+
---
+++ darkice/tags/darkice-0_13/src/OssDspSource.cpp
@@ -0,0 +1,338 @@
+/*------------------------------------------------------------------------------
+
+   Copyright (c) 2000 Tyrell Corporation. All rights reserved.
+
+   Tyrell DarkIce
+
+   File     : OssDspSource.cpp
+   Version  : $Revision$
+   Author   : $Author$
+   Location : $Source$
+   
+   Copyright notice:
+
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License  
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+   
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of 
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the 
+    GNU General Public License for more details.
+   
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+------------------------------------------------------------------------------*/
+
+/* ============================================================ include files */
+
+#include "OssDspSource.h"
+
+#ifdef SUPPORT_OSS_DSP
+// only compile this code if there is support for it
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#else
+#error need unistd.h
+#endif
+
+#ifdef HAVE_STRING_H
+#include <string.h>
+#else
+#error need string.h
+#endif
+
+#ifdef HAVE_SYS_TYPES_H
+#include <sys/types.h>
+#else
+#error need sys/types.h
+#endif
+
+#ifdef HAVE_SYS_STAT_H
+#include <sys/stat.h>
+#else
+#error need sys/stat.h
+#endif
+
+#ifdef HAVE_FCNTL_H
+#include <fcntl.h>
+#else
+#error need fcntl.h
+#endif
+
+#ifdef HAVE_SYS_TIME_H
+#include <sys/time.h>
+#else
+#error need sys/time.h
+#endif
+
+#ifdef HAVE_SYS_IOCTL_H
+#include <sys/ioctl.h>
+#else
+#error need sys/ioctl.h
+#endif
+
+#ifdef HAVE_SYS_SOUNDCARD_H
+#include <sys/soundcard.h>
+#else
+#error need sys/soundcard.h
+#endif
+
+
+#include "Util.h"
+#include "Exception.h"
+#include "OssDspSource.h"
+
+
+/* ===================================================  local data structures */
+
+
+/* ================================================  local constants & macros */
+
+/*------------------------------------------------------------------------------
+ *  File identity
+ *----------------------------------------------------------------------------*/
+static const char fileid[] = "$Id$";
+
+
+/* ===============================================  local function prototypes */
+
+
+/* =============================================================  module code */
+
+/*------------------------------------------------------------------------------
+ *  Tell if source id big endian
+ *----------------------------------------------------------------------------*/
+bool
+OssDspSource :: isBigEndian ( void ) const                  throw ()
+{
+    return AFMT_S16_NE == AFMT_S16_BE;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Initialize the object
+ *----------------------------------------------------------------------------*/
+void
+OssDspSource :: init (  const char      * name )    throw ( Exception )
+{
+    fileName       = Util::strDup( name);
+    fileDescriptor = 0;
+    running        = false;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  De-initialize the object
+ *----------------------------------------------------------------------------*/
+void
+OssDspSource :: strip ( void )                      throw ( Exception )
+{
+    if ( isOpen() ) {
+        close();
+    }
+    
+    delete[] fileName;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Open the audio source
+ *----------------------------------------------------------------------------*/
+bool
+OssDspSource :: open ( void )                       throw ( Exception )
+{
+    int             format;
+    int             i;
+    unsigned int    u;
+
+    if ( isOpen() ) {
+        return false;
+    }
+
+    switch ( getBitsPerSample() ) {
+        case 8:
+            format = AFMT_U8;
+            break;
+
+        case 16:
+            format = AFMT_S16_NE;
+            break;
+            
+        default:
+            return false;
+    }
+
+    if ( (fileDescriptor = ::open( fileName, O_RDONLY)) == -1 ) {
+        fileDescriptor = 0;
+        return false;
+    }
+
+    i = format;
+    if ( ioctl( fileDescriptor, SNDCTL_DSP_SETFMT, &i) == -1 ||
+         i != format ) {
+        
+        close();
+        throw Exception( __FILE__, __LINE__, "can't set format", i);
+    }
+
+    u = getChannel();
+    if ( ioctl( fileDescriptor, SNDCTL_DSP_CHANNELS, &u) == -1 ||
+         u != getChannel() ) {
+        
+        close();
+        throw Exception( __FILE__, __LINE__, "can't set channels", u);
+    }
+
+    u = getSampleRate();
+    if ( ioctl( fileDescriptor, SNDCTL_DSP_SPEED, &u) == -1 ) {
+
+        close();
+        throw Exception( __FILE__, __LINE__,
+                         "can't set soundcard recording sample rate", u);
+    }
+    if ( u != getSampleRate() ) {
+        reportEvent( 2, "sound card recording sample rate set to ", u,
+                        " while trying to set it to ", getSampleRate());
+        reportEvent( 2, "this is probably not a problem, but a slight "
+                        "drift in the sound card driver");
+    }
+
+    return true;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Check wether read() would return anything
+ *----------------------------------------------------------------------------*/
+bool
+OssDspSource :: canRead ( unsigned int    sec,
+                          unsigned int    usec )    throw ( Exception )
+{
+    fd_set              fdset;
+    struct timeval      tv;
+    int                 ret;
+
+    if ( !isOpen() ) {
+        return false;
+    }
+
+    if ( !running ) {
+        /* ugly workaround to get the dsp into recording state */
+        unsigned char * b =
+                          new unsigned char[getChannel()*getBitsPerSample()/8];
+        read( b, getChannel()*getBitsPerSample()/8);
+        delete[] b;
+    }
+    
+    FD_ZERO( &fdset);
+    FD_SET( fileDescriptor, &fdset);
+    tv.tv_sec  = sec;
+    tv.tv_usec = usec;
+
+    ret = select( fileDescriptor + 1, &fdset, NULL, NULL, &tv);
+    
+    if ( ret == -1 ) {
+        throw Exception( __FILE__, __LINE__, "select error");
+    }
+
+    return ret > 0;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Read from the audio source
+ *----------------------------------------------------------------------------*/
+unsigned int
+OssDspSource :: read (    void          * buf,
+                          unsigned int    len )     throw ( Exception )
+{
+    ssize_t     ret;
+
+    if ( !isOpen() ) {
+        return 0;
+    }
+
+    ret = ::read( fileDescriptor, buf, len);
+
+    if ( ret == -1 ) {
+        throw Exception( __FILE__, __LINE__, "read error");
+    }
+
+    running = true;
+    return ret;
+}
+
+
+/*------------------------------------------------------------------------------
+ *  Close the audio source
+ *----------------------------------------------------------------------------*/
+void
+OssDspSource :: close ( void )                  throw ( Exception )
+{
+    if ( !isOpen() ) {
+        return;
+    }
+
+    ::close( fileDescriptor);
+    fileDescriptor = 0;
+    running        = false;
+}
+
+#endif // SUPPORT_OSS_DSP
+
+
+/*------------------------------------------------------------------------------
+ 
+  $Source$
+
+  $Log$
+  Revision 1.12  2002/12/20 10:40:40  darkeye
+  added support for big endian OSS devices (like Linux PowerPC)
+
+  Revision 1.11  2002/05/28 12:35:41  darkeye
+  code cleanup: compiles under gcc-c++ 3.1, using -pedantic option
+
+  Revision 1.10  2001/09/26 16:55:30  darkeye
+  BSD port
+
+  Revision 1.9  2001/09/11 15:05:21  darkeye
+  added Solaris support
+
+  Revision 1.8  2001/09/02 14:08:40  darkeye
+  setting the sound card recording sample rate is now more relaxed
+  there is no error reported if the sample rate is not exactly the same
+
+  Revision 1.7  2001/08/30 17:25:56  darkeye
+  renamed configure.h to config.h
+
+  Revision 1.6  2000/12/01 15:03:28  darkeye
+  bug fix in error reporting
+
+  Revision 1.5  2000/11/17 15:50:48  darkeye
+  added -Wall flag to compiler and eleminated new warnings
+
+  Revision 1.4  2000/11/13 20:05:07  darkeye
+  changed to workaround to start recording so that it reads one sample
+  per channel, as opposed to only one sample (which misalignes the channels)
+
+  Revision 1.3  2000/11/12 13:31:40  darkeye
+  added kdoc-style documentation comments
+
+  Revision 1.2  2000/11/05 14:08:28  darkeye
+  changed builting to an automake / autoconf environment
+
+  Revision 1.1.1.1  2000/11/05 10:05:53  darkeye
+  initial version
+
+  
+------------------------------------------------------------------------------*/
+
