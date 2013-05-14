94dd6d7a8015c133fd48c8ba96716da39ddaaabd
rafael@riseup.net <rafael@riseup.net@caa462fc-51d2-11de-8b28-e1b452efc53a>
2013-05-14 17:42:27 +0000

    branch 0.7 created

---
+++ darkice-gui/branches/darksnow-0.7/config_files.h
@@ -0,0 +1,25 @@
+/* Darksnow - A GUI for darkice
+ * Copyright (C) 2004-2005 Rafael Diniz <rafael@riseup.net>
+ *
+ * This source code is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Public License as published 
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * This source code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * Please refer to the GNU Public License for more details.
+ *
+ * You should have received a copy of the GNU Public License along with
+ * this source code; if not, write to:
+ * Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+#define __CONFIG_FILES_H__
+
+/* writes darksnow config file to disk. if opt != 0, save in path, else, save in darksnow config directory */
+gboolean darksnow_config_store( char opt, char *save_path);
+
+gboolean darksnow2darkice_cfg();
